bb72ff5ec33eeb31be8905ed8907bcf40377a664
Ayomide Bakare <bakkareayomideo@gmail.com>
2019-03-27 11:16:45 +0100

    sweet sweet open source

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# Base image
+FROM node:10.15.0
+
+# Create and set the work directory
+WORKDIR /ayomidebakare.site-blog
+
+# Copy app to WORKDIR
+COPY . /ayomidebakare.site-blog
+
+# Globally install dependencies we may need manually to run from the cli
+RUN npm i -g gatsby-cli@2.4.17
+
+# Install dependencies
+RUN npm i
+
+# Mount the node_modules directory as a volume to bind it at run time
+VOLUME ["/ayomidebakare.site-blog"]
+
+EXPOSE 5553
+
+# Specify the command(s) to be run (at runtime :))
+CMD bash
---
+++ config/website.js
@@ -0,0 +1,29 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
+  siteTitle: 'Ayomide Bakare', // Navigation and Site Title
+  siteTitleAlt: 'The personal site of Ayomide Bakare', // Alternative Site title for SEO
+  siteTitleShort: 'ayomidebakare', // short_name for manifest
+  siteUrl: 'https://ayomidebakare.site', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription: 'Hey! Learn from my experiences in building a career in technology',
+  author: 'Ayomide Bakare', // Author for schemaORGJSONLD
+  organization: 'Unparked LLC',
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@mbao01', // Twitter Username
+  ogSiteName: 'Ayomide Bakare', // Facebook Site Name
+  ogLanguage: 'en_US',
+  googleAnalyticsID: '',
+
+  // Manifest and Progress color
+  themeColor: '#5348FF',
+  backgroundColor: '#2b2e3c',
+
+  // Social component
+  twitter: 'https://twitter.com/mbao01/',
+  twitterHandle: '@mbao01',
+  github: 'https://github.com/mbao01/',
+  gitlab: 'https://gitlab.com/mbao01/',
+  linkedin: 'https://linkedin.com/in/ayomidebakare/',
+}
