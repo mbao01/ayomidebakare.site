f9e4ff81cc150339605d3614b25be47cfe8dfecb
Ayomide Bakare <bakkareayomideo@gmail.com>
2019-11-18 12:00:39 +0100

    update manifest for pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         url: '/about',
       },
     ],
-    image: config.siteLogo,
+    image: config.siteLogoCircle,
     author: {
       name: config.author,
       minibio: `
@@ -37,7 +37,7 @@ module.exports = {
     organization: {
       name: config.organization,
       url: config.siteUrl,
-      logo: config.siteLogo,
+      logo: config.siteLogoCircle,
     },
     social: {
       handle: config.handle,
@@ -103,14 +103,40 @@ module.exports = {
         display: 'standalone',
         icons: [
           {
-            src: '/favicons/android-chrome-192x192.png',
-            sizes: '192x192',
+            src: '/favicons/android-icon-36x36.png',
+            sizes: '36x36',
             type: 'image/png',
+            density: '0.75',
           },
           {
-            src: '/favicons/android-chrome-384x384.png',
-            sizes: '384x384',
+            src: '/favicons/android-icon-48x48.png',
+            sizes: '48x48',
             type: 'image/png',
+            density: '1.0',
+          },
+          {
+            src: '/favicons/android-icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            density: '1.5',
+          },
+          {
+            src: '/favicons/android-icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            density: '2.0',
+          },
+          {
+            src: '/favicons/android-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            density: '3.0',
+          },
+          {
+            src: '/favicons/android-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            density: '4.0',
           },
         ],
       },
--- src/config/website.js
+++ src/config/website.js
@@ -6,6 +6,7 @@ module.exports = {
   siteUrl: 'https://ayomidebakare.site', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteLogoCircle: 'images/logo-circle.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
   siteDescription:
     'Hey! Learn from my experiences in building a career in technology',
   author: 'Ayomide Bakare', // Author for schemaORGJSONLD
